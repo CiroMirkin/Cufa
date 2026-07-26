5c81e08632313844edc00502ee2e3459a0c3dc16
CiroMirkin <ciromirkin@gmail.com>
2026-07-26 01:01:01 -0300

    refactor: rename subject path params

--- app/src/routes/home/home.tsx
+++ app/src/routes/home/home.tsx
@@ -15,8 +15,8 @@ function Home() {
                 {subjects?.map((subject) => (
                     <Link
                         key={subject.id}
-                        to="/subject/$subjectName"
-                        params={{ subjectName: subject.name }}
+                        to="/subject/$subjectId"
+                        params={{ subjectId: subject.id }}
                         className="px-8 py-4 rounded text-xl bg-green-100 hover:bg-green-200 shadow-sm"
                     >
                         {subject.name}
--- app/src/routes/subject/$subjectId.tsx
+++ app/src/routes/subject/$subjectId.tsx
@@ -1,21 +1,21 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
 import { ChevronLeft } from 'lucide-react'
 
-export const Route = createFileRoute('/subject/$subjectName')({
+export const Route = createFileRoute('/subject/$subjectId')({
   component: Subject,
 })
 
 function Subject() {
-  const { subjectName } = Route.useParams()
+  const { subjectId } = Route.useParams()
   return (
     <div className="px-6">
       <header className='w-full flex justify-start py-4'>
         <Link to='/' className='flex items-center text-xl'>
-          <ChevronLeft className='mr-2' size={26} /> { subjectName } - Carrera
+          <ChevronLeft className='mr-2' size={26} /> { subjectId } - Carrera
         </Link>
       </header>
       <main className='pt-4'>
-    
+        <Outlet />
       </main>
     </div>
   )
