548407ae8864d824ea088974eeb464564719667a
ciromirkin@gmail.com <ciromirkin@gmail.com>
2026-07-27 17:14:58 -0300

    refactor: hide document list and adjust back navigation when viewing a document

--- app/src/routes/subject/$subjectId.tsx
+++ app/src/routes/subject/$subjectId.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Link, Outlet, useChildMatches } from '@tanstack/react-router'
 import { ChevronLeft } from 'lucide-react'
 import { DocumentsList } from '@/components/DocumentsList'
 
@@ -8,15 +8,28 @@ export const Route = createFileRoute('/subject/$subjectId')({
 
 function Subject() {
   const { subjectId } = Route.useParams()
+  const childMatches = useChildMatches()
+  const hasChildRoute = childMatches.length > 0
+
   return (
     <div className="px-6">
       <header className='w-full flex justify-start py-4'>
-        <Link to='/' className='flex items-center text-xl'>
-          <ChevronLeft className='mr-2' size={26} /> { subjectId } - Carrera
-        </Link>
+        {hasChildRoute ? (
+          <Link
+            to='/subject/$subjectId'
+            params={{ subjectId }}
+            className='flex items-center text-xl'
+          >
+            <ChevronLeft className='mr-2' size={26} /> {subjectId} - Carrera
+          </Link>
+        ) : (
+          <Link to='/' className='flex items-center text-xl'>
+            <ChevronLeft className='mr-2' size={26} /> {subjectId} - Carrera
+          </Link>
+        )}
       </header>
       <main className='pt-4'>
-        <DocumentsList subjectId={subjectId} />
+        {!hasChildRoute && <DocumentsList subjectId={subjectId} />}
         <Outlet />
       </main>
     </div>
