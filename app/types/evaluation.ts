a35ca4a7bfe13e511eb00716e5706b025a4b4133
CiroMirkin <ciromirkin@gmail.com>
2026-08-13 15:19:40 -0300

    feat: create evaluation type

--- app/types/evaluation.ts
+++ app/types/evaluation.ts
@@ -1,14 +1,14 @@
-
-export type EvaluationType = "partial" | "final" | "retake" | "practical_work" | "presentation"
+export type EvaluationType = "partial" | "final" | "retake" | "practical_work" | "presentation" | "task"
 
 export interface Evaluation {
   id: string
   subjectId: string
 
   title: string
-  note: string
+  note?: string
   type: EvaluationType
 
   date: string
-  link: string
+  link?: string
+  topics?: string[]
 }
