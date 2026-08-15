5b2ee8e540a8a4cd98e9a1dd58a78b78d28dd819
ciromirkin@gmail.com <ciromirkin@gmail.com>
2026-08-15 11:42:30 -0300

    feat: add save icon

--- app/constants/icons.ts
+++ app/constants/icons.ts
@@ -8,6 +8,7 @@ import library from "@/assets/icons/library.svg"
 import archive from "@/assets/icons/archive.svg"
 import plus from "@/assets/icons/plus.svg"
 import clock_plus from "@/assets/icons/clock-plus.svg"
+import save from "@/assets/icons/device-floppy.svg"
 
 export const icons = {
     home,
@@ -20,6 +21,7 @@ export const icons = {
     plus,
     archive,
     clock_plus,
+    save,
 } as const;
 
 export type IconKey = keyof typeof icons
