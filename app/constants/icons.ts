02a73ecd5d417bc454dc73d5b74de27f989ab038
CiroMirkin <ciromirkin@gmail.com>
2026-08-16 23:14:35 -0300

    feat: add new icons

--- app/constants/icons.ts
+++ app/constants/icons.ts
@@ -9,6 +9,11 @@ import archive from "@/assets/icons/archive.svg"
 import plus from "@/assets/icons/plus.svg"
 import clock_plus from "@/assets/icons/clock-plus.svg"
 import save from "@/assets/icons/device-floppy.svg"
+import chevron_right from "@/assets/icons/chevron-right.svg"
+import link from "@/assets/icons/link.svg"
+import user from "@/assets/icons/user.svg"
+import close from "@/assets/icons/x.svg"
+import settings from "@/assets/icons/settings.svg"
 
 export const icons = {
     home,
@@ -22,6 +27,11 @@ export const icons = {
     archive,
     clock_plus,
     save,
+    chevron_right,
+    close,
+    settings,
+    user,
+    link,
 } as const;
 
 export type IconKey = keyof typeof icons
