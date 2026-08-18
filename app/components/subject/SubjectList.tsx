8986b65978e7cb3cd8dec1eb00dfce6abb6d6b35
CiroMirkin <ciromirkin@gmail.com>
2026-08-18 20:10:22 -0300

    feat: show upcoming schedule label in SubjectList

--- app/components/subject/SubjectList.tsx
+++ app/components/subject/SubjectList.tsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native"
 import { Link } from "expo-router"
 import { Subject } from "@/types/subject"
 import { icons } from "@/constants/icons"
+import { getUpcomingScheduleLabel } from "@/lib/schedule"
 
 interface Props {
   subjects: Subject[]
@@ -13,25 +14,31 @@ export default function SubjectList({ subjects, onDelete }: Props) {
 
   return (
     <View className="w-full border-2 rounded-lg mt-4 overflow-hidden">
-      {subjects.map((item, index) => (
-        <Link
-          key={item.id}
-          href={{
-            pathname: "/(tabs)/(subject)/[id]",
-            params: { id: item.id },
-          }}
-          className={`bg-orange ${
-            index !== subjects.length - 1 ? "border-b-2 border-black" : ""
-          }`}
-        >
-          <View className="w-full flex-row items-center justify-between p-4">
-            <Text className="text-xl font-medium text-black">
-              {item.name}
-            </Text>
-            <icons.chevron_right width={24} height={24} />
-          </View>
-        </Link>
-      ))}
+      {subjects.map((item, index) => {
+        const scheduleLabel = getUpcomingScheduleLabel(item.schedules)
+
+        return (
+          <Link
+            key={item.id}
+            href={{
+              pathname: "/(tabs)/(subject)/[id]",
+              params: { id: item.id },
+            }}
+            className={`bg-orange ${index !== subjects.length - 1 ? "border-b-2 border-black" : ""
+              }`}
+          >
+            <View className="w-full flex-row items-center justify-between p-4">
+              <View>
+                {scheduleLabel && <Text className="text-xs">{scheduleLabel}</Text>}
+                <Text className="text-xl font-medium text-black">
+                  {item.name}
+                </Text>
+              </View>
+              <icons.chevron_right width={24} height={24} />
+            </View>
+          </Link>
+        )
+      })}
     </View>
   )
 }
