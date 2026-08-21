18bf537ee3165cf44422d47c7374c744a1ddd7e5
CiroMirkin <ciromirkin@gmail.com>
2026-08-21 19:39:13 -0300

    feat: add yellow color

--- app/components/evaluation/getProximityStyle.tsx
+++ app/components/evaluation/getProximityStyle.tsx
@@ -3,7 +3,7 @@ export function getProximityStyle(date: Date) {
   const diff = date.getTime() - Date.now();
   const days = diff / (1000 * 60 * 60 * 24);
   if (days <= 4) return { bg: "bg-red", border: "border-2", };
-  if (days <= 7) return { bg: "bg-[#fdf28b]", border: "border-2", };
+  if (days <= 7) return { bg: "bg-yellow", border: "border-2", };
   if (days <= 14) return { bg: "bg-green", border: "border-2", };
   return { bg: "bg-white", border: "border-2", };
 }
